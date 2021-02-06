e0dc46670a04ebaac7929ed29887f7f85b4d8eb0
avaquinn <ava.isobel.quinn@gmail.com>
2021-02-06 10:32:01 -0700

    added move functionatlity

--- snake.py
+++ snake.py
@@ -1,16 +1,45 @@
 from sense_hat import SenseHat
 
+import time
+
 sense = SenseHat()
 
 class Snake:
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+
     def __init__(self, path, direction):
         self.path = path
         self.direction = direction
 
     def draw(self):
-       white = (255, 255, 255)
        for pixel in self.path:
-           sense.set_pixel(pixel[0], pixel[1], white)
+            sense.set_pixel(pixel[0], pixel[1], self.white)
+   
+    def move(self, direction):
+        self.remove_tail()
+        self.add_head(direction)
+
+
+    def add_head(self, direction):
+        old_head = self.path[0]
+        if direction == 'w':
+            new_head = (old_head[0] -1, old_head[1])
+        elif direction == 'n':
+            new_head = (old_head[0], old_head[1] +1)
+        elif direction == 'e':
+            new_head = (old_head[0] +1, old_head[1])
+        elif direction == 's':
+            new_head = (old_head[0], old_head[1] -1)
+        else :
+            raise Exception("Invalid direction")
+        self.path.insert(0,new_head)
+        sense.set_pixel(new_head[0], new_head[1], self.white)
+
+    def remove_tail(self):
+       tail = self.path[-1]
+       sense.set_pixel(tail[0], tail[1], self.black)
+       path = self.path[:-1]
 
 
 sense.clear((0, 0, 0))
@@ -18,8 +47,5 @@ brian = Snake([(5,4),(6,4),(7,4)],'w')
 brian.draw()
 
 
-
-
-
-
-
+time.sleep(2)
+brian.move('w')
